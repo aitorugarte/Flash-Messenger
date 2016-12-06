00abcf45a5d4312730de481ec0c41da87349c300
aitorugarte <aitorugarte@opendeusto.es>
2016-12-06 13:49:41 +0100

    Actualizados cifrados

--- Flash Messenger/src/ClienteV2/Encriptado/Principal.java
+++ Flash Messenger/src/ClienteV2/Encriptado/Principal.java
@@ -3,15 +3,20 @@ package ClienteV2.Encriptado;
 public class Principal {
 
 	public static void main(String[] args) throws Exception {
-
+		
+		int codigo = 5; //codigo
 		String llave = "92AE31A79FEEB2A3"; // llave
 		String vi = "0123456789ABCDEF"; // vector de inicialización
-		String texto = "hola";
+		String texto = "hola"; //Texto a cifrar
 		
-		System.out.println("Texto normal " + texto);
-		System.out.println("Texto encriptado: " + AES.encriptar(llave, vi, texto));
-		System.out.println("Texto desencriptado: "
-				+ AES.decrypt(llave, vi, AES.encriptar(llave, vi, texto)));
+		System.out.println("   .:Cifrado César:. "
+				+ "\nTexto normal: " + texto
+				+ "\nTexto cifrado: " + TiposCifrado.cifrarCesar(texto, codigo));
+		System.out.println();
+		System.out.println("   .:Cifrado AES:."
+				+ "\nTexto normal: " + texto
+				+ "\nTexto cifrado: " + TiposCifrado.encriptar(llave, vi, texto));
+		
 	}
 
 }
--- Flash Messenger/src/ClienteV2/Encriptado/TiposCifrado.java
+++ Flash Messenger/src/ClienteV2/Encriptado/TiposCifrado.java
@@ -1,5 +1,12 @@
 package ClienteV2.Encriptado;
 
+import static org.apache.commons.codec.binary.Base64.decodeBase64;
+import static org.apache.commons.codec.binary.Base64.encodeBase64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 public class TiposCifrado {
 
 	private static char[] minus = {'a','b','c','d','e','f','g','h','i','j',
@@ -10,16 +17,6 @@ public class TiposCifrado {
     								'K','L','M','N','Ñ','O','P','Q','R',
     								'S','T','U','V','W','X','Y','Z'};
     
-    
-	public static void main(String[] args) {
-		
-		int codigo = 5;
-		String palabra = cifrarCesar("HoLa QuE TaL", codigo );
-		System.out.println("Palabra cifrada: " + palabra);
-		System.out.println(descifrarCesar(palabra, codigo));
-	}
-
-	
 	/*
 	 * Cifrado César E(x) = x + n mod(27)
 	 * La posición de la letra en el abcdario + el código % 27
@@ -51,7 +48,10 @@ public class TiposCifrado {
 		return String.valueOf(letras);
 		
 	}
-	
+	/*
+	 * Descifrado César E(x) = x - n mod(27)
+	 * La posición de la letra en el abcdario - el código % 27
+	 */
 	public static String descifrarCesar(String palabra, int codigo){
 		
 		char[] letras = palabra.toCharArray();
@@ -79,5 +79,40 @@ public class TiposCifrado {
 		return String.valueOf(letras);
 	}
 	
+	 /**
+     * Función de tipo String que recibe una llave (key), un vector de inicialización (vi)
+     * y el texto que se desea cifrar
+     * @param llave la llave en tipo String a utilizar
+     * @param vi el vector de inicialización a utilizar
+     * @param texto el texto sin cifrar a encriptar
+     * @return el texto cifrado en modo String
+     *@throws Exception puede devolver excepciones de muchos tipos     
+     **/
+    public static String encriptar(String llave, String vi, String texto) throws Exception {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec skeySpec = new SecretKeySpec(llave.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(vi.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivParameterSpec);
+            byte[] encrypted = cipher.doFinal(texto.getBytes());
+            return new String(encodeBase64(encrypted));
+    }
 
+    /**
+     * Función de tipo String que recibe una llave (key), un vector de inicialización (iv)
+     * y el texto que se desea descifrar
+     * @param llave la llave en tipo String a utilizar
+     * @param vi el vector de inicialización a utilizar
+     * @param encriptado el texto cifrado en modo String
+     * @return el texto desencriptado en modo String
+     * @throws Exception puede devolver excepciones de muchos tipos
+     **/
+    public static String decrypt(String llave, String vi, String encriptado) throws Exception {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            SecretKeySpec skeySpec = new SecretKeySpec(llave.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(vi.getBytes());
+            byte[] enc = decodeBase64(encriptado);
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, ivParameterSpec);
+            byte[] decrypted = cipher.doFinal(enc);
+            return new String(decrypted);
+    }
 }
